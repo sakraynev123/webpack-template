5019a9aeb789cfca357a424b432ddc20defe17ee
Alexander <klivets.alex@gmail.com>
2023-07-05 19:53:33 +0300

    feat: add editorconfig, eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import './index.html';
 import './index.scss';
-import { mult, sum } from './modules/cals';
-
-console.log(mult(3,5));
-console.log(sum(3,5));
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/[name][ext]'
+    assetModuleFilename: 'assets/[name][ext]',
   },
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, 'src', 'index.html')
@@ -56,6 +56,36 @@ module.exports = {
         type: 'asset/resource',
         generator: {
           filename: 'fonts/[name][ext]',
+        }
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]'
         }
       },
       {
